99bedafc08754175735bc6764b9706269e79992d
orinno <oktavianirianti2017@gmail.com>
2025-01-18 11:49:43 +0700

    add Prisma integration with user model and serverless support

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,55 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+// import { NestFactory, Reflector } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import {
+//   initializeTransactionalContext,
+//   StorageDriver,
+// } from 'typeorm-transactional';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// import { ClassSerializerInterceptor } from '@nestjs/common';
+
+// async function bootstrap() {
+//   initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+//   app.setGlobalPrefix('api/');
+
+//   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+//   app.enableCors();
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  initializeTransactionalContext,
-  StorageDriver,
-} from 'typeorm-transactional';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { Handler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
+
+let server: Handler;
 
 async function bootstrap() {
-  initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+  const app = await NestFactory.create(AppModule);
 
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.setGlobalPrefix('api/');
+  // Add global prefix if necessary
+  app.setGlobalPrefix('api'); // Optional
 
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  await app.init();
 
-  app.enableCors();
-
-  await app.listen(process.env.PORT ?? 3000);
+  const expressApp = app.getHttpAdapter().getInstance();
+  server = serverlessExpress({ app: expressApp });
 }
+
 bootstrap();
+
+export const handler: Handler = async (event, context) => {
+  if (!server) {
+    await bootstrap();
+  }
+  return server(event, context, (error, result) => {
+    if (error) {
+      throw error;
+    }
+    return result;
+  });
+};
